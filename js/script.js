ca4d79650d610b9b3c63179102376307b80aa49d
epaniagua2000 <epaniagua@hawk.iit.edu>
2023-03-29 20:26:16 -0500

    Alert, text change works once

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  
+    var words = document.getElementById('entryinput').placeholder;
 
-
-
-
-
-
-
+    function myEventFunction() {
+      var temp = document.getElementsByTagName('h2').item(0);
+      var name = temp.innerHTML;
+      alert(name + ': ' + words);
+    }
+    
+    var div = document.getElementById('entrybutton');
+    var output = document.getElementById('textoutput');
+    
+    div.addEventListener('click', myEventFunction);
+    div.addEventListener('click', function(){
+      output.innerHTML = words;
+    });
+    
+}
 window.addEventListener('load', init);
